5cd699ee93eab2ece90ec76635b19c02714bd197
awwkoala <karolinam.m@wp.pl>
2021-06-20 14:01:06 +0200

    Removed comment

--- src/main/java/com/github/awwkoala/encryption/util/StringCharArrayConverter.java
+++ src/main/java/com/github/awwkoala/encryption/util/StringCharArrayConverter.java
@@ -3,10 +3,6 @@ package com.github.awwkoala.encryption.util;
 public class StringCharArrayConverter {
     public char[] toCharArray(String string) {
         char[] charArray = string.toCharArray();
-        /*char[] charArray = new char[string.length()];
-        for (int i = 0; i < string.length(); i++) {
-            charArray[i] = string.charAt(i);
-        }*/
         return charArray;
     }
     public String toString(char[] charArray) {
